e64da1a446f304546c9f0ca0995ee816d046d080
Pritush kr Singh <mynameispritush.15@gmail.com>
2023-05-11 14:36:02 +0530

    done with the exercise1 of django

--- mysite1/mysite1/views.py
+++ mysite1/mysite1/views.py
@@ -2,8 +2,12 @@
 """we have to give an http response becz the views retuens an http response"""
 
 from django.http import HttpResponse
+html_string=""
+with open('mysite1/execise1.html', 'r') as f:
+    html_string+=(f.read())
+
 def index(request): # ye index takes an argument by default which is request
-    return HttpResponse("<h1>HELLO</h1>") # using an html here
+    return HttpResponse(html_string) # using an html here
 
 def about(request): # koi bhi function not only the index takes an argument by default which is request
     return HttpResponse("about hello")

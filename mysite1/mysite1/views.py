8fe0308ca70925dcedecd295c64953a8b92a9672
Pritush kr Singh <mynameispritush.15@gmail.com>
2023-05-13 22:57:01 +0530

    added the index.html using the template and adding a functionality to it half way

--- mysite1/mysite1/views.py
+++ mysite1/mysite1/views.py
@@ -16,30 +16,25 @@ def about(request): # koi bhi function not only the index takes an argument by d
     return HttpResponse("about hello")
 """# this is the exercise1 code
 
-
-"""Back button is made by <a herf='/'>back</a> this a tag"""
-
 #adding an back button
-html_string=""
-with open('mysite1/backbutton.html', 'r') as f:
-    html_string+=(f.read())
+"""Back button is made by <a href='/'>back</a> this a tag"""
+
 
 def index(request):
-    params = {"name":"Pritush","Place":"Out of the world"}
-    return render(request,'index.html',params)
+    return render(request,'index.html')
 # this uses the html file named index in the index folder which is being displayed in the web this is the easier way to manage the pages
 
 def rm_punctua(request):
-    return HttpResponse(html_string)
+    return HttpResponse("""<a href='/'>back</a>""")
 
 def capital(response):
-    return HttpResponse(html_string)
+    return HttpResponse("<a href='/'> back </a>")
 
 def newline_remove_first(response):
-    return HttpResponse(html_string)
+    return HttpResponse("<a href='/'>back</a>")
 
 def space_remover(response):
-    return HttpResponse(html_string)
+    return HttpResponse("<a href='/'>back</a>")
 
 def charcount(response):
-    return HttpResponse(html_string)
+    return HttpResponse("<a href='/'>back</a>")
